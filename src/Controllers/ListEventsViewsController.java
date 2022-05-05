8aa0af3e4b05f903ef68a4e790b91911b4a3fd65
Endezoumou Fozame <armandbryan572@icloud.com>
2022-05-05 04:52:21 +0100

    Interface event principal
    Signed-off-by: Endezoumou Fozame <armandbryan572@icloud.com>

--- src/Controllers/ListEventsViewsController.java
+++ src/Controllers/ListEventsViewsController.java
@@ -7,7 +7,13 @@ package Controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 
 /**
  * FXML Controller class
@@ -16,9 +22,24 @@ import javafx.fxml.Initializable;
  */
 public class ListEventsViewsController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private VBox cardEvent;
+
+    @FXML
+    private ImageView eventimg;
+
+    @FXML
+    private GridPane grid;
+
+    @FXML
+    private Label namecategorieprincipal;
+
+    @FXML
+    private Label nameeventprincipal;
+
+    @FXML
+    private ScrollPane scroll;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
